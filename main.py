f489911d963298441957e30268dfb4e957dcc539
Alobanov123 <149011404+Alobanov123@users.noreply.github.com>
2024-03-28 19:26:34 +0000

    Doing a google search in python

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello World!')
+# Start of Code from Geeksforgeeks
+try:
+  from googlesearch import search
+except ImportError: 
+  print("No module named 'google' found")
+
+# to search
+query = "Geeksforgeeks"
+
+for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+  print(j)
+
+# End of Code from Geeksforgeeks
