86a75ebf36ac842966fdf4cca98e2daccba37a9b
aalter1 <148587809+aalter1@users.noreply.github.com>
2024-04-28 05:28:34 +0000

    Graphs fully implemented, deleted old python files

--- dashboard.py
+++ dashboard.py
@@ -1,38 +1,52 @@
 # reference https://github.com/plotly/tutorial-code/blob/main/Videos/matplotlib-dashboard.py
-from dash import Dash, html, dcc, Input, Output  # pip install dash
-import plotly.express as px
-import dash_ag_grid as dag                       # pip install dash-ag-grid
-import dash_bootstrap_components as dbc          # pip install dash-bootstrap-components
-import pandas as pd                              # pip install pandas
 
-import matplotlib                                # pip install matplotlib
+# https://stackoverflow.com/questions/59001244/python-dash-app-updating-with-new-dataframe
+from dash import Dash, Input, Output, dcc, html
+import dash_ag_grid as dag
+import dash_bootstrap_components as dbc
+from dash.exceptions import PreventUpdate
+
+import os
+import pandas as pd
+import numpy as np
+import serpapi
+
+import plotly.express as px
+import matplotlib
+
 matplotlib.use('agg')
-import matplotlib.pyplot as plt
 import base64
 from io import BytesIO
 
-df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/solar.csv")
+import matplotlib.pyplot as plt
+
+google_api = os.environ['google_api']
+client = serpapi.Client(api_key=google_api)
+
+df = pd.read_csv('scraping.csv')
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = dbc.Container([
-    html.H1("Our template", className='mb-2', style={'textAlign':'center'}),
+    html.H1("Shopping Tool", className='mb-2', style={'textAlign':'center'}),
+
+    dbc.Row([
+      dcc.Input(
+          id='query',
+          type= 'text',
+          placeholder="Enter a product you would like to search",
+          required=True,
+          debounce=True
+      )
+    ]),
 
     dbc.Row([
         dbc.Col([
             dcc.Dropdown(
                 id='category',
-                value='Number of Solar Plants',
+                value='price',
                 clearable=False,
-                options=[{'label': col, 'value': col} for col in df.columns[1:]]
+              options=[{'label': col, 'value': col} for col in df.columns[1:]]
             )
-        ], width=4),
-        dbc.Col([
-            dcc.Input(
-                id='input-textbox',
-                type='text',
-                placeholder='Enter value...',
-                debounce=True,
-            ),
         ], width=4)
     ]),
 
@@ -43,71 +57,114 @@ app.layout = dbc.Container([
     ]),
 
     dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='bar-graph-plotly', figure={})
-        ], width=12, md=6),
-        dbc.Col([
-            dag.AgGrid(
-                id='grid',
-                rowData=df.to_dict("records"),
-                columnDefs=[{"field": i} for i in df.columns],
-                columnSize="sizeToFit",
-            )
-        ], width=12, md=6),
-    ], className='mt-4'),
+      dag.AgGrid(
+        id='grid',
+        rowData=df.to_dict('records'),
+        columnDefs=[{"field": i} for i in df.columns],
+        columnSize="sizeToFit")
+    ]),
 
+    dbc.Row([
+      dcc.Graph(id='bar-graph-plotly', figure={}),
+    ])
 ])
 
-# Callback to store the text input value in the global variable
+# Populate table with data
 @app.callback(
-    Output('input-textbox', 'value'),
-    [Input('input-textbox', 'value')]
+  Output('grid','rowData'),
+  Input('query','value'),
 )
-def store_text_input_value(value):
-    global global_text_input
-    global_text_input = value
 
-    # Return None to prevent the input box from being updated visually
-    return None
+def query_csv(query):
+  results = client.search({
+    'engine': 'google_shopping',
+    'q': query,
+    'num': '100',
+  })
+
+  # A list of all wanted attributes
+  arg_list = ['title',
+              'extracted_price',
+              'rating']
+
+  arg_list = np.array(arg_list)
+
+  # Create a nested list of all the products wanted attributes
+  shopping_results = []
+
+  # Iterate through the results
+  for product in results['shopping_results']:
+    p_list = []
+    # Append the wanted attributes to list p_list
+    for arg in arg_list:
+      # Removes commas from string objects
+      # Ignores if the item does not have the attribute
+      try:
+        if isinstance(product[arg], str):
+          product[arg] = product[arg].replace(',', '')
+
+        # Removes .com from source to make data more uniform
+        if arg == 'source':
+          product[arg] = product[arg].replace('.com', '')
+        p_list.append(product[arg])
+      #  If the attribute is not in the product dictionary, add NA to the list
+      except KeyError:
+        p_list.append(0)
+
+    # Append the list p_list to the list shopping_results that contains all the products
+    shopping_results.append(p_list)
+
+  # Create a data frame from the nested list and add it to a csv file
+  df = pd.DataFrame(shopping_results, columns=arg_list)
+  header_str = ','.join(arg_list)
+  file = np.savetxt('scraping.csv', df, delimiter = ',', fmt = '%s', header = header_str)
+  dff = pd.read_csv('scraping.csv')
+  # fill the table with data
+  return dff.to_dict('records')
+
 # Create interactivity between dropdown component and graph
 @app.callback(
-    Output(component_id='bar-graph-matplotlib', component_property='src'),
-    Output('bar-graph-plotly', 'figure'),
-    Output('grid', 'defaultColDef'),
-    Input('category', 'value'),
+  Output(component_id='bar-graph-matplotlib', component_property='src'),
+  Output('bar-graph-plotly', 'figure'),
+  Output('grid', 'defaultColDef'),
+  Input('category', 'value'),
+  Input('grid', 'rowData'),
 )
-def plot_data(selected_yaxis):
 
-    # Build the matplotlib figure
-    fig = plt.figure(figsize=(14, 5))
-    plt.plot(df['State'], df[selected_yaxis])
-    plt.ylabel(selected_yaxis)
-    plt.xticks(rotation=30)
+def plot_data(selected_cat, data):
+  
+   # Build the matplotlib figure
+  
+   fig = plt.figure(figsize=(14, 5))
+   df = pd.DataFrame(data)
+   num_bins = 10
+   plt.hist(df[selected_cat], bins=num_bins, edgecolor='black')
+   plt.ylabel('Number of Items')
 
-    # Save it to a temporary buffer.
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
-    # Embed the result in the html output.
-    fig_data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    fig_bar_matplotlib = f'data:image/png;base64,{fig_data}'
+   # Save it to a temporary buffer.
+   buf = BytesIO()
+   fig.savefig(buf, format="png")
+   # Embed the result in the html output.
+   fig_data = base64.b64encode(buf.getbuffer()).decode("ascii")
+   fig_hist_matplotlib = f'data:image/png;base64,{fig_data}'
 
-    # Build the Plotly figure
-    fig_bar_plotly = px.line(df, x='State', y=selected_yaxis).update_xaxes(tickangle=330)
+   # Build the Plotly figure
+   fig_box_plotly = px.box(df, x=selected_cat, labels = {'Value': 'Value', 'Count': 'Number of Items'})
 
-    my_cellStyle = {
-        "styleConditions": [
-            {
-                "condition": f"params.colDef.field == '{selected_yaxis}'",
-                "style": {"backgroundColor": "#d3d3d3"},
-            },
-            {   "condition": f"params.colDef.field != '{selected_yaxis}'",
-                "style": {"color": "black"}
-            },
-        ]
-    }
+   my_cellStyle = {
+       "styleConditions": [
+           {
+               "condition": f"params.colDef.field == '{selected_cat}'",
+               "style": {"backgroundColor": "#d3d3d3"},
+           },
+           {   "condition": f"params.colDef.field != '{selected_cat}'",
+               "style": {"color": "black"}
+           },
+       ]
+   }
 
-    return fig_bar_matplotlib, fig_bar_plotly, {'cellStyle': my_cellStyle}
+   return fig_hist_matplotlib, fig_box_plotly, {'cellStyle': my_cellStyle}
 
 
 if __name__ == '__main__':
-   app.run_server(debug=True, host='0.0.0.0', port='8050')
+   app.run(debug=False)
--- scraping.py
+++ scraping.py
@@ -4,48 +4,12 @@ import numpy as np
 import pandas as pd
 import serpapi
 
-# Create a list that orders the results by price
-
-def sort_by_price(results):
-  my_list = []
-  for product in results['shopping_results']:
-    price = product['price'].replace('$', '')
-    price = float(price.replace(',', ''))
-    my_list.append(price)
-  my_list.sort()
-  return(my_list)
-
-# Function that returns the product with the lowest price
-
-def find_min_product(results, dict):
-  my_products = []
-  price = sort_by_price(results)[0]
-  print(price)
-  for product in dict:
-    if dict[product] == price:
-      my_products.append(product)
-  return(my_products)
-
-# Creates a dictionary of the product names and prices
-
-def create_dict(results):
-  price_dict = {}
-  for product in results['shopping_results']:
-    price = product['price']
-    price = price.replace('$', '')
-    price = price.replace(',', '')
-    price = float(price)
-    price_dict[product['title']] = price
-  return(price_dict)
-
-
-# user_input = input("Enter a search term: ")
-
 # From youtube video: scraping shopping tab using google api
 
 google_api = os.environ['google_api']
 client = serpapi.Client(api_key=google_api)
 
+  
 results = client.search({
   'engine': 'google_shopping',
   'q': 'phone',
@@ -53,18 +17,11 @@ results = client.search({
 })
 
 # A list of all wanted attributes
-arg_list = ['position',
-            'title', 
-            'source', 
-            'price',
+arg_list = ['title',
             'extracted_price',
-            'old_price',
-            'delivery',
-            'thumbnail',
-            'rating',
-            'reviews',
-            'store_rating',
-            'store_reviews']
+            'rating']
+
+arg_list = np.array(arg_list)
 
 # Create a nested list of all the products wanted attributes
 shopping_results = []
@@ -79,20 +36,22 @@ for product in results['shopping_results']:
     try:
       if isinstance(product[arg], str):
         product[arg] = product[arg].replace(',', '')
-
       # Removes .com from source to make data more uniform
       if arg == 'source':
         product[arg] = product[arg].replace('.com', '')
+      elif arg == 'extracted_price':
+        product[arg] = int(product[arg])
+        
       p_list.append(product[arg])
     #  If the attribute is not in the product dictionary, add NA to the list
     except KeyError:
-      p_list.append('NA')
+      p_list.append(0)
   
   # Append the list p_list to the list shopping_results that contains all the products
   shopping_results.append(p_list)
 
 # Create a data frame from the nested list and add it to a csv file
-df = pd.DataFrame(shopping_results) 
-
+df = pd.DataFrame(shopping_results, columns = arg_list) 
 header_str = ','.join(arg_list)
 file = np.savetxt('scraping.csv', df, delimiter = ',', fmt = '%s', header = header_str)
+
